cdfac010344e36d5c268a0eb81fbae22b0ae80d5
Cola <boss@colaio.com>
2021-08-21 15:25:38 +0800

    Create enum.go

---
+++ enum/enum.go
@@ -0,0 +1,7 @@
+package enum
+
+const (
+	QUEUE_POSTOFFICE__SIGNUP_WELCOME string = "io.douya.postoffice.signup-welcome"
+	QUEUE_POSTOFFICE__VERIFY_ACCOUNT string = "io.douya.postoffice.verify-account"
+	QUEUE_POSTOFFICE__RESET_PASSWORD string = "io.douya.postoffice.reset-password"
+)
